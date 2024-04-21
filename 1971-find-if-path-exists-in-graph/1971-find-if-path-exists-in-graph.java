4027bf912a2e9e33868c5add841ae03b5aee345b
AnkitaMannaGH <130164520+AnkitaMannaGH@users.noreply.github.com>
2024-04-21 10:22:36 +0530

    Time: 42 ms (84.41%), Space: 128.6 MB (75.65%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,40 @@
+class Solution {
+    List<Integer>[] graph;
+
+    public boolean validPath(int numberOfNodes, int[][] edges, int source, int destination) {
+        createGraph(numberOfNodes, edges);
+        
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[numberOfNodes];
+        queue.add(source);
+        visited[source] = true;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            if (current == destination) {
+                return true;
+            }
+
+            List<Integer> neighbours = graph[current];
+            for (int node : neighbours) {
+                if (!visited[node]) {
+                    visited[node] = true;
+                    queue.add(node);
+                }
+            }
+        }
+        return false;
+    }
+
+    private void createGraph(int numberOfNodes, int[][] edges) {
+        graph = new ArrayList[numberOfNodes];
+        for (int i = 0; i < numberOfNodes; ++i) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+    }
+}
