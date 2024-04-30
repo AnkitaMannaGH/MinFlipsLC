429a3fd1dfd5bcc65f4027b14e3c3dc0788a569e
AnkitaMannaGH <130164520+AnkitaMannaGH@users.noreply.github.com>
2024-04-30 17:58:46 +0530

    Time: 20 ms (68.87%), Space: 45.1 MB (72.64%) - LeetHub

---
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long wonderfulSubstrings(String word) {
+        long[] count = new long[1024];
+        count[0]=1;
+        int cur = 0;
+        long res=0L;
+        for(char c: word.toCharArray()) {
+            int idx= c-'a';
+            cur^=1<<idx;
+            res+=count[cur]++;
+            for(int i=0;i<10;i++) {
+                int temp = cur ^ (1<<i);
+                res+=count[temp];
+            }
+        }
+        return res;
+    }
+}
