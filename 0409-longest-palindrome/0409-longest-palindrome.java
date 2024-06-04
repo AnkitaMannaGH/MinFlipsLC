dd08d7f839818e3f4e530f37302c66fe8d5694a6
AnkitaMannaGH <130164520+AnkitaMannaGH@users.noreply.github.com>
2024-06-04 10:31:01 +0530

    Time: 1 ms (100.00%), Space: 41.7 MB (46.48%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int longestPalindrome(String s) {
+       int[] count = new int['z' - 'A' + 1];
+		for (char c : s.toCharArray()) 
+            count[c - 'A']++;
+		int odd = 0;
+		for (char c = 'A'; c <= 'z'; c++) 
+            odd += (count[c - 'A'] & 1);
+		return s.length() - odd + (odd > 0 ? 1 : 0); 
+    }
+}
